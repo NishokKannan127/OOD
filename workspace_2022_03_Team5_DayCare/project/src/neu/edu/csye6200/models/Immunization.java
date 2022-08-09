64bcdce226aab128acc1b3c10c4aa150664cbd1a
sneharavichandran <ravichandran.sn@northeastern.edu>
2022-08-09 19:59:47 -0400

    Immunization logic

--- workspace_2022_03_Team5_DayCare/project/src/neu/edu/csye6200/models/Immunization.java
+++ workspace_2022_03_Team5_DayCare/project/src/neu/edu/csye6200/models/Immunization.java
@@ -1,5 +1,21 @@
 package neu.edu.csye6200.models;
 
+import java.util.HashMap;
+
 public class Immunization {
+	HashMap<String,Integer> vaccineMap;
+	public void Immunization()
+	{
+		vaccineMap=new HashMap<>();
+	}
+	public HashMap<String, Integer> getVaccineMap() {
+		return vaccineMap;
+	}
+
+	public void setVaccineMap(HashMap<String, Integer> vaccineMap) {
+		this.vaccineMap = vaccineMap;
+	}
+	
+	
 
 }
